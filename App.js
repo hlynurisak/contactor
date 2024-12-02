66463e0ec2e032a3eeb5cb4de707e70c2a91d435
Hlynur Isak <isakvillason@hotmail.com>
2024-12-02 12:48:46 +0000

    Add not functioning search bar

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import ContactsList from './src/components/ContactsList'; 
+import SearchBar from './src/components/SearchBar';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <SearchBar />
       <ContactsList /> 
       <StatusBar style="auto" />
     </View>
